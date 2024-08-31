0e6d0bbe35c7903e901ac425e13c258d5ddf743b
SamuelVIC862 <samuca862@gmail.com>
2024-08-31 15:19:55 -0300

    Projetos

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -2,12 +2,10 @@ const AboutContainer = () => {
     return <section className="about-cont">
         <h2>Sobre</h2>
         <p>
-            Olá me chamo Samuel Victor, sou um Desenvolvedor Web Front-End,
-             tenho atualmente 22 anos, residente de Natal/RN. Sempre fui um grande
-             apaixonado por essa área, poder criar e usar a criatividade para desenvolver
-             aplicações que vão ajudar as pessoas, é um objetivo que me faz a cada dia
-             crescer e aprender, tanto por novas tecnologias quanto pelas já conhecidas,
-             sempre com o objetivo de evoluir.
+        Olá, meu nome é Samuel Victor, sou um Desenvolvedor Web Front-End com 22 anos, residente em Natal/RN.
+         Desde cedo, descobri uma paixão por desenvolvimento web, onde posso aliar criatividade e tecnologia para criar aplicações que realmente fazem a diferença na vida das pessoas.
+          O desafio de transformar ideias em soluções digitais me motiva a continuar evoluindo e aprendendo, seja explorando novas tecnologias ou aprimorando meu conhecimento nas ferramentas que já domino.
+         Estou sempre em busca de crescimento pessoal e profissional, com o objetivo de me tornar cada vez mais competente e inovador na minha área.
         </p>
     </section>
 
--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,26 +1,32 @@
-
+import Project1 from '../img/image.png'
+import Project0 from '../img/projectbases.png'
 
 import '../styles/components/projectscontainer.sass'
 
 const projects = [
-    {id:"1", name:"", image:"", desc:""},
-]
+    { id: "1", name: "Portfólio", image: Project1, link:"https://github.com/SamuelVIC862/Portfolio_Samuel", desc: "Este projeto de portfólio foi desenvolvido para apresentar, de forma detalhada, meus conhecimentos e habilidades como desenvolvedor web. Nele, você encontrará uma visão abrangente das minhas competências técnicas, experiência em diferentes tecnologias, e exemplos de projetos nos quais trabalhei, destacando minha capacidade de criar soluções web eficazes e inovadoras." },
+    { id: "2", name: "Em Construção", image: Project0, link:"https://github.com/SamuelVIC862", desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." },
+    //{id:"3", name:"Test", image:"", link:"https://github.com/SamuelVIC862", desc:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"},
+    ]
 
 const ProjectsContainer = () => {
-  return <section className="projects_cont">
-    <h2>Projetos</h2>
-    <div className='projects_grid'>
+    return <section className="projects_cont">
+        <h2>Projetos</h2>
+        <div className='projects_grid'>
             {projects.map((project) => (
-                <div className="project_card" id={project.id} key={project.id}>
-                    <img src={project.image} alt="" />
+                <a href={project.link} className="project_card" id={project.id} key={project.id}>
+                    <div className="box_img" style={{
+                        backgroundImage: `url(${project.image})`
+                    }}>
+                    </div>
                     <div className="project_info">
                         <h3>{project.name}</h3>
                         <p>{project.desc}</p>
                     </div>
-                </div>
+                </a>
             ))}
         </div>
-  </section>
+    </section>
 }
 
 export default ProjectsContainer
